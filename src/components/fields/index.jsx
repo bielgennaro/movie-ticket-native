eb33136dd46764b797eca882e7a63e72d4c8f032
Jose Sanches <josesanches@xbrain.com.br>
2023-08-17 09:05:21 -0300

    add component Fields

---
+++ src/components/fields/index.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { TextInput, Text, StyleSheet } from "react-native";
+
+export const Fields = ({ values }) => {
+  const [fields, setFields] = useState(values);
+  const [error, setError] = useState({});
+
+  const handleFieldChange = (fieldName, fieldProps, text) => {
+    setFields((prevFields) => ({
+      ...prevFields,
+      [fieldName]: { ...prevFields[fieldName], value: text },
+    }));
+    validateField(fieldName, fieldProps, text);
+  };
+
+  const validateField = (fieldName, fieldProps, fieldValue) => {
+    let newError = { ...error };
+
+    if (fieldProps.email) {
+      const eRegex = /^([a-zA-Z0-9_.+-]+)@([a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)$/;
+      newError[fieldName] = eRegex.test(fieldValue) ? "" : "E-mail inválido";
+    }
+
+    if (fieldProps.password) {
+      newError[fieldName] = fieldValue.length >= 6 ? "" : "Senha inválida";
+    }
+
+    if (fieldProps.required && fieldValue === "") {
+      newError[fieldName] = "Campo obrigatório";
+    }
+
+    setError(newError);
+  };
+
+  return (
+    <>
+      {Object.keys(fields).map((fieldName) => {
+        const fieldProps = fields[fieldName];
+        const fieldError = error[fieldName];
+
+        return (
+          <React.Fragment key={fieldName}>
+            <TextInput
+              placeholder={fieldProps.placeholder}
+              style={styles.textInput}
+              placeholderTextColor="#fff"
+              onChangeText={(text) =>
+                handleFieldChange(fieldName, fieldProps, text)
+              }
+              value={fieldProps.value}
+              secureTextEntry={fieldProps.password}
+            />
+            <Text style={{ color: "red" }} key={`error[${fieldName}]`}>
+              {fieldError}
+            </Text>
+          </React.Fragment>
+        );
+      })}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  textInput: {
+    backgroundColor: "transparent",
+    color: "#fff",
+    textAlign: "center",
+    borderBottomColor: "#d9232a",
+    borderBottomWidth: 1,
+    width: "80%",
+    height: 40,
+  },
+});
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,18 +1,37 @@
-import { SafeAreaView, View, Text, TextInput, StyleSheet } from "react-native";
+import { useMemo, useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+} from "react-native";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { Fields } from "../../components/forms";
 
 export const Login = ({ navigation }) => {
+  const values = {
+    email: {
+      required: true,
+      email: true,
+      placeholder: "E-mail",
+    },
+    password: {
+      required: true,
+      password: true,
+      placeholder: "Senha",
+    },
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <MaterialIcons name={"account-circle"} size={100} color={"#fff"} />
-        <TextInput placeholder="Email" style={styles.textInput} />
-        <TextInput
-          placeholder="Password"
-          secureTextEntry={true}
-          textContentType="password"
-          style={styles.textInput}
-        />
+      <View style={styles.viewContainer}>
+        <MaterialIcons name="account-circle" size={200} color="#fff" />
+        <Fields values={values} />
+        <View>
+          <Button title="Entrar" color="#d9232a" />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -22,8 +41,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#000",
+    color: "#fff",
   },
   textInput: {
-    backgroundColor: "#fff",
+    backgroundColor: "transparent",
+    color: "#fff",
+    textAlign: "center",
+    borderBottomColor: "#d9232a",
+    borderBottomWidth: 1,
+    width: "80%",
+    height: 40,
+  },
+  viewContainer: {
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    gap: 20,
   },
 });
